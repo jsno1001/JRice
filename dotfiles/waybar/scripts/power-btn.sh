8d2fa25a17c99ad7640115844efbfe3d1fd37845
jsno1001 <jsno5555@gmail.com>
2026-03-30 03:28:44 +0300

    fix

--- dotfiles/waybar/scripts/power-btn.sh
+++ dotfiles/waybar/scripts/power-btn.sh
@@ -1,6 +1,6 @@
 result=$(echo -e "Poweroff \nReboot \nWiFi \n" | wofi --dmenu --width 500 --height 400)
 if [ $result == "Poweroff" ]; then
-    Poweroff
+    poweroff
 elif [ $result == "Reboot" ]; then
     reboot
 elif [ $result == "WiFi" ]; then
